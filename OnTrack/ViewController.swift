5c47ade071e969134ef430361285513b98a7e2ff
Patrick Ma <fivetwentysix@gmail.com>
2018-07-03 14:10:08 -0700

    Initial Commit

---
+++ OnTrack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OnTrack
+//
+//  Created by Patrick Ma on 7/3/18.
+//  Copyright © 2018 fivetwentysix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

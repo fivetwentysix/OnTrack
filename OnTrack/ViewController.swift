9a98154317f03302ffb55471e758179056ea5324
Patrick Ma <fivetwentysix@gmail.com>
2018-07-03 16:49:02 -0700

    add to list on enter

--- OnTrack/ViewController.swift
+++ OnTrack/ViewController.swift
@@ -9,11 +9,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet var textField: NSTextField!
+    @IBOutlet var table: NSTableView!
+    var list: [String]!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.list = []
+        self.table.delegate = self as? NSTableViewDelegate
+        self.table.dataSource = self
     }
 
     override var representedObject: Any? {
@@ -21,7 +25,34 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    override func controlTextDidEndEditing(_ obj: Notification) {
+        if !textField.stringValue.isEmpty {
+            list.append(textField.stringValue)
+            reloadData()
+        }
+        textField.stringValue = ""
+    }
+    
+    func reloadData() {
+        table.reloadData();
+    }
 }
 
+extension ViewController: NSTableViewDataSource{
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        var result:NSTableCellView
+        result  = tableView.makeView(withIdentifier: (tableColumn?.identifier)!, owner: self) as! NSTableCellView
+        if (tableColumn?.title == "Task") {
+            result.textField!.stringValue = list[row]
+            return result
+        } else {
+            result.textField!.stringValue = list[row]
+            return result
+        }
+    }
+}
